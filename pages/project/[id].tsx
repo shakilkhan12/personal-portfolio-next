49411cfb78a6b352d7aee2780b28ef3b8e59a42a
shakilkhan12 <shakilkhan621@gmail.com>
2022-11-05 18:35:46 +0500

    used getServerSideProps

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -7,27 +7,29 @@ import Header from '../../components/Header'
 import Data from "../../data/projects.json"
 import Footer from '../../components/Footer'
 interface Projects {
-  id: number;
+  project: {
+    id: number;
   title: string;
   stack: string[];
   description: string;
   link: string;
   code: string;
+  }
 }
-const project = () => {
-  const {query, back} = useRouter();
-  const [details, setDetails] = useState<Projects>();
-  const id: number | any = query.id;
-  useEffect(() => {
-    const response: Projects | any = Data.find(pro => pro.id == id);
-    setDetails(response)
-  }, [id])
-  console.log(details)
+const project = ({project}: Projects) => {
+  const {back} = useRouter();
+  // const [details, setDetails] = useState<Projects>();
+  // const id: number | any = query.id;
+  // useEffect(() => {
+  //   const response: Projects | any = Data.find(pro => pro.id == id);
+  //   setDetails(response)
+  // }, [id])
+  // console.log(props.)
   return (
     <div>
          <Head>
-        <title>{details && details.title}</title>
-        <meta name="description" content={details && details.description} />
+        <title>{project.title}</title>
+        <meta name="description" content={project.description} />
         <link rel="icon" href="/images/javascript.png" />
       </Head>
       <div className='max-w-screen-lg mx-auto py-20 px-5'>
@@ -36,10 +38,10 @@ const project = () => {
           <div className='text-orange-500 flex items-center capitalize mb-2 cursor-pointer' onClick={() => back()}>
            <IoIosArrowRoundBack size={22} /> go back
           </div>
-          {details && <>
-            <h1 className='text-2xl text-white font-bold'>{details.title}</h1>
+          {<>
+            <h1 className='text-2xl text-white font-bold'>{project.title}</h1>
             <div className='flex flex-wrap -mx-2 my-3'>
-              {details.stack.length > 0 && details.stack.map((stack: string, index) => (
+              {project.stack.length > 0 && project.stack.map((stack: string, index) => (
                 <span key={index} className="p-2">
                   <span className='bg-zinc-800 text-white px-4 py-1.5 uppercase font-medium rounded-full text-xs'>{stack}</span>
                 </span>
@@ -47,13 +49,13 @@ const project = () => {
             </div>
             <div>
               <li className='text-orange-500'>
-                <a href={details.link} className="text-orange-500 capitalize" target="_blank">live demo</a>
+                <a href={project.link} className="text-orange-500 capitalize" target="_blank">live demo</a>
               </li>
               <li className='text-orange-500'>
-                <a href={details.code} className="text-orange-500 capitalize" target="_blank">source code</a>
+                <a href={project.code} className="text-orange-500 capitalize" target="_blank">source code</a>
               </li>
             </div>
-            <p className='text-gray-400 mt-3'>{details.description}</p>
+            <p className='text-gray-400 mt-3'>{project.description}</p>
           </>}
           
         </div>
@@ -64,3 +66,11 @@ const project = () => {
 }
 
 export default project
+export const getServerSideProps = ({query}: any) =>  {
+      const response: Projects | any = Data.find(pro => pro.id == query.id);
+      return {
+        props: {
+          project: response
+        }
+      }
+}
